bbdc83794a032387a830073f132a21e9ca3520ec
Umme Hani <uh.snigdha@gmail.com>
2024-10-18 22:41:36 +0600

    i++ changed into i+1, desired result not found

--- prb_solving.js
+++ prb_solving.js
@@ -3,20 +3,20 @@ let findSecondLargestElement = (arr) => {
 
     for (let i = 0; i < arr.length; i++) {
 
-        if (arr[i] > arr[i++]) {
+        if (arr[i] > arr[i+1]) {
             largest = arr[i];
 
-            if(arr[i] !== largest, arr[i] > arr[i++]) {
+            if(arr[i] !== largest, arr[i] > arr[i+1]) {
                 console.log(arr[i]);
             }
-            else if (arr[i] !== largest, arr[i] == arr[i++], arr[i+2] > arr[i++]) {
+            else if (arr[i] !== largest, arr[i] == arr[i+1], arr[i+2] > arr[i+1]) {
                 console.log(arr[i+2]);
             }
             else {
                 console.log(-1);
             }
         }
-        console.log(arr);
+        //console.log(arr);
     }
 }
 
