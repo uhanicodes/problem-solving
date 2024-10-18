4e061b68f3f7fc7beeb529cbeb7b1fe2283a3e6d
Umme Hani <uh.snigdha@gmail.com>
2024-10-18 22:02:45 +0600

    unsolved problem

---
+++ prb_solving.js
@@ -0,0 +1,23 @@
+let findSecondLargestElement = (arr) => {
+    let largest = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] > arr[i++]) {
+            largest = arr[i];
+
+            if(arr[i] !== largest, arr[i] > arr[i++]) {
+                console.log(arr[i]);
+            }
+            else if (arr[i] !== largest, arr[i] == arr[i++], arr[i+2] > arr[i++]) {
+                console.log(arr[i+2]);
+            }
+            else {
+                console.log(-1);
+            }
+        }
+        console.log(arr);
+    }
+}
+
+findSecondLargestElement([17, 12, 34, 45, 90]);
