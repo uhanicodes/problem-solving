0a0c5dc61bca806cb4b53e2541a0c201ddad9f20
Umme Hani <uh.snigdha@gmail.com>
2024-11-22 11:13:46 +0600

    linked-list

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -15,3 +15,39 @@ while (current != null) {
     console.log(current.val);
     current = current.next;
 }
+
+function Student(name, roll) {
+    this.name = name;
+    this.roll = roll;
+}
+
+console.log(new Student("Abid", "01"));
+
+function LinkedList(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+let a1 = new LinkedList(100, undefined);
+let a2 = new LinkedList(200, a1);
+let a3 = new LinkedList(500, a2);
+
+console.log(a3);
+
+console.log(new LinkedList(100, undefined));
+
+let x = {
+    val : 100,
+    next : undefined
+}
+
+console.log(x);
+
+// a3 => a2 => a1
+
+let curr = a3;
+
+while (curr !== null) {
+    console.log("curr node:", curr);
+    curr = curr.next;
+}
