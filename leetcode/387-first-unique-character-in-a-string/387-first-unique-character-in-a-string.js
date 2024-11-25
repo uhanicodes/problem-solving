258f6f67e87b096cc561c034fa92cc7c8d7a7a78
Umme Hani <uh.snigdha@gmail.com>
2024-11-26 01:07:43 +0600

    modified

--- leetcode/387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ leetcode/387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -1,12 +1,26 @@
 var firstUniqChar = function(s) {
    
     for (let i = 0; i < s.length; i++) {
-        
-        for (let j = i + 1; j < s.length; j++) {
+        let found = false;
 
+        for (let j = i + 1; j < s.length; j++) {
+            
             if (s[i] == s[j]) {
+                found = true;
                 break;
             }
         }
+
+        if (found == false) {
+            return i;
+        } 
    } 
+
+   return -1;
 };
+
+console.log(firstUniqChar("leetcode"));
+
+console.log(firstUniqChar("loveleetcode"));
+
+console.log(firstUniqChar("aabb"));
