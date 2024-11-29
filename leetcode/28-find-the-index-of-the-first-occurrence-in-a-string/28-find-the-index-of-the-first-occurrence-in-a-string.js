73516f85a7e407b9c3680e3b85c2914eee7f7241
Umme Hani <uh.snigdha@gmail.com>
2024-11-29 23:49:33 +0600

    index of first element

--- leetcode/28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.js
+++ leetcode/28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -1,3 +1,18 @@
+//go to each element of needle
+//if each element of needle are found in haystack serially return the index of first occurance of first element
 var strStr = function(haystack, needle) {
     
+    for (let i = 0; i < haystack.length; i++) {
+
+        for (let j = 0; j < needle.length; j++) {
+            
+            if (needle[j] == haystack[i]) {
+                
+            }
+        }
+    }
 };
+
+//haystack = "sadbutsad", needle = "sad"
+//haystack = "leetcode", needle = "leeto"
+//haystack = "leetcode", needle = "code"
