949c4b81dc32f4a035a70ee40e4fd1fac85b7e09
Umme Hani <uh.snigdha@gmail.com>
2024-12-07 12:32:51 +0600

    added toString

--- leetcode/412-fizz-buzz/412-fizz-buzz.js
+++ leetcode/412-fizz-buzz/412-fizz-buzz.js
@@ -2,21 +2,21 @@ var fizzBuzz = function(n) {
     let arr = [];
 
     for(let i = n - 1; i > -1; i--) {
-        arr[i] = n;
+        arr[i] = n.toString();
         n--;
 
         if (arr[i] % 3 == 0 && arr[i] % 5 == 0) {
             arr[i] = "FizzBuzz";
         }
         if (arr[i] % 3 == 0) {
-            arr[i] = "Buzz";
+            arr[i] = "Fizz";
         }
         if (arr[i] % 5 == 0) {
-            arr[i] = "Fizz";
+            arr[i] = "Buzz";
         }
     }
 
     return arr;
 };
 
-console.log(fizzBuzz(15));
+console.log(fizzBuzz(5));
