006ed4a05d31f120670048845053e042257700ca
Umme Hani <uh.snigdha@gmail.com>
2024-12-14 01:46:05 +0600

    solved

--- leetcode/2717-semi-ordered-permutation/2717-semi-ordered-permutation.js
+++ leetcode/2717-semi-ordered-permutation/2717-semi-ordered-permutation.js
@@ -1,20 +1,26 @@
 var semiOrderedPermutation = function(nums) {
     let swap1 = 0;
     let swap2 = 0;
-    let swap = swap1 + swap2;
+    let swap = 0;
     let n = nums.length;
+
+    if (n == 1) {
+        return swap;
+    }
 
     for (let i = 0; i < n; i++) {
         
         if(nums[i] == 1) {
             swap1 = i;
+            swap = swap + swap1;
         }
 
         if(nums[i] == n) {
             swap2 = n - 1 - i;
+            swap = swap + swap2;
         }
     }
     return swap;    
 };
 
-console.log(semiOrderedPermutation([2,1,4,3]))
+console.log(semiOrderedPermutation([2, 3, 1, 4]))
