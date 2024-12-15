612d035fd580d582006303bbb886b541974bd9b5
Umme Hani <uh.snigdha@gmail.com>
2024-12-16 00:31:06 +0600

    m

--- leetcode/735-asteroid-collision/735-asteroid-collision.js
+++ leetcode/735-asteroid-collision/735-asteroid-collision.js
@@ -1,31 +1,44 @@
 var asteroidCollision = function(asteroids) {
 
-    for (let i = 0; i < asteroids.length; i++) {
+    while (true) {
 
-        if (asteroids[i] < 0 && (asteroids[i - 1] || asteroids[i + 1]) >= 0) {
+        for (let i = 0; i < asteroids.length; i++) {
 
-            if(Math.abs(asteroids[i]) == Math.abs(asteroids[i + 1])) {
-                asteroids.splice(i, 1);
-                asteroids.splice(i + 1, 1);
-            }
+            if (asteroids[i] < 0 && (asteroids[i - 1] || asteroids[i + 1]) >= 0) {
 
-            if (Math.abs(asteroids[i]) == Math.abs(asteroids[i - 1])) {
-                asteroids.splice(i, 1);
-                asteroids.splice(i - 1, 1);
-            }
+                if(Math.abs(asteroids[i]) == Math.abs(asteroids[i + 1])) {
+                    asteroids.splice(i, 1);
+                    asteroids.splice(i + 1, 1);
+                }
 
-            if(Math.abs(asteroids[i]) < Math.abs(asteroids[i + 1])) {
-                asteroids.splice(i, 1);
-            }
-            else {
-                asteroids.splice(i + 1, 1);
-            }
+                if (Math.abs(asteroids[i]) == Math.abs(asteroids[i - 1])) {
+                    asteroids.splice(i, 1);
+                    asteroids.splice(i - 1, 1);
+                }
 
-            if(Math.abs(asteroids[i]) < Math.abs(asteroids[i - 1])) {
-                asteroids.splice(i, 1);
+                if(Math.abs(asteroids[i]) < Math.abs(asteroids[i + 1])) {
+                    asteroids.splice(i, 1);
+                }
+                else {
+                    asteroids.splice(i + 1, 1);
+                }
+
+                if(Math.abs(asteroids[i]) < Math.abs(asteroids[i - 1])) {
+                    asteroids.splice(i, 1);
+                }
+                else {
+                    asteroids.splice(i - 1, 1);
+                }
             }
-            else {
-                asteroids.splice(i - 1, 1);
+        }
+
+        for (let i = 0; i < asteroids.length; i++) {
+            
+            if (asteroids[i] >= 0 && asteroids[i + 1] < 0) {
+                break;
+            }
+            else if (asteroids[i] < 0 && asteroids[i + 1] >= 0) {
+                break;
             }
         }
     }
