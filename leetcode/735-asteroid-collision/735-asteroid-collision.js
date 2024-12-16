c001bae5b065bd59de0dc2a6015fdde7df30d6ba
Umme Hani <uh.snigdha@gmail.com>
2024-12-16 23:25:35 +0600

    m

--- leetcode/735-asteroid-collision/735-asteroid-collision.js
+++ leetcode/735-asteroid-collision/735-asteroid-collision.js
@@ -4,8 +4,7 @@ var asteroidCollision = function(asteroids) {
 
         for (let i = 0; i < asteroids.length - 1; i++) {
 
-            if ((asteroids[i] < 0 &&  asteroids[i + 1] >= 0) || 
-                (asteroids[i] >= 0 &&  asteroids[i + 1] < 0)) {
+            if (asteroids[i] >= 0 &&  asteroids[i + 1] < 0) {
 
                 if(Math.abs(asteroids[i]) == Math.abs(asteroids[i + 1])) {
                     asteroids.splice(i, 2);
