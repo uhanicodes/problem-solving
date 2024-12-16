72418f9847724d0b9a4d6f1d49c555c99014ec42
Umme Hani <uh.snigdha@gmail.com>
2024-12-16 21:58:26 +0600

    m

--- leetcode/735-asteroid-collision/735-asteroid-collision.js
+++ leetcode/735-asteroid-collision/735-asteroid-collision.js
@@ -2,18 +2,13 @@ var asteroidCollision = function(asteroids) {
 
     while (true) {
 
-        for (let i = 0; i < asteroids.length; i++) {
+        for (let i = 0; i < asteroids.length - 1; i++) {
 
-            if (asteroids[i] < 0 && (asteroids[i - 1] || asteroids[i + 1]) >= 0) {
+            if ((asteroids[i] < 0 &&  asteroids[i + 1] >= 0) || 
+                (asteroids[i] >= 0 &&  asteroids[i + 1] < 0)) {
 
                 if(Math.abs(asteroids[i]) == Math.abs(asteroids[i + 1])) {
-                    asteroids.splice(i, 1);
-                    asteroids.splice(i + 1, 1);
-                }
-
-                if (Math.abs(asteroids[i]) == Math.abs(asteroids[i - 1])) {
-                    asteroids.splice(i, 1);
-                    asteroids.splice(i - 1, 1);
+                    asteroids.splice(i, 2);
                 }
 
                 if(Math.abs(asteroids[i]) < Math.abs(asteroids[i + 1])) {
@@ -22,15 +17,10 @@ var asteroidCollision = function(asteroids) {
                 else {
                     asteroids.splice(i + 1, 1);
                 }
-
-                if(Math.abs(asteroids[i]) < Math.abs(asteroids[i - 1])) {
-                    asteroids.splice(i, 1);
-                }
-                else {
-                    asteroids.splice(i - 1, 1);
-                }
             }
         }
+
+        //console.log(asteroids);
 
         let sameSign = true;
 
@@ -56,4 +46,4 @@ var asteroidCollision = function(asteroids) {
     
 };
 
-console.log(asteroidCollision([-5, 2, 10]));
+console.log(asteroidCollision([-2,-1,1,2]));
