811f18c17e463c89090e0308baa63ff4eb11af01
Umme Hani <uh.snigdha@gmail.com>
2024-12-17 22:00:33 +0600

    m

--- leetcode/874-walking-robot-simulation/874-walking-robot-simulation.js
+++ leetcode/874-walking-robot-simulation/874-walking-robot-simulation.js
@@ -1,14 +1,16 @@
 var robotSim = function(commands, obstacles) {
-  
-    let (x, y) = (0, 0);
+    let x = 0, y = 0;
+    let direction = "N";
 
     for (let i = 0; i < commands.length; i++) {
 
         if (commands[i] == -1) {
             // turn 90 degrees right.
+            direction = "E";
         }
         else if (commands[i] == -2) {
             // turn 90 degrees left.
+            direction = "W";
         }
         else if (commands[i] > 0) {
             // move forward commands[i] times from the current position, once at a time.
@@ -16,3 +18,5 @@ var robotSim = function(commands, obstacles) {
 
     }
 };
+
+console.log(robotSim([4,-1,3], []));
