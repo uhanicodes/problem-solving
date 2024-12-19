81c811ad4448f73d3a67cf07fb9b94e1cf06e532
Umme Hani <uh.snigdha@gmail.com>
2024-12-19 15:25:59 +0600

    m

--- leetcode/874-walking-robot-simulation/874-walking-robot-simulation.js
+++ leetcode/874-walking-robot-simulation/874-walking-robot-simulation.js
@@ -73,7 +73,7 @@ var robotSim = function(commands, obstacles) {
                 else if (direction == "S") {
                     for (let k = 0; k < obstacles.length; k++) {
                         
-                        if (x == obstacles[k][0] && (y + 1) == obstacles[k][1]) {
+                        if (x == obstacles[k][0] && (y - 1) == obstacles[k][1]) {
                             isObstacle = true;
                         }
                     }
@@ -88,7 +88,7 @@ var robotSim = function(commands, obstacles) {
                 else if (direction == "W") {
                     for (let k = 0; k < obstacles.length; k++) {
                         
-                        if ((x + 1) == obstacles[k][0] && y == obstacles[k][1]) {
+                        if ((x - 1) == obstacles[k][0] && y == obstacles[k][1]) {
                             isObstacle = true;
                         }
                     }
