7d8850c818e91f38fce6676ee9ad509db0861ae2
Umme Hani <uh.snigdha@gmail.com>
2024-12-21 22:14:35 +0600

    m

--- leetcode/874-walking-robot-simulation/874-walking-robot-simulation.js
+++ leetcode/874-walking-robot-simulation/874-walking-robot-simulation.js
@@ -1,9 +1,21 @@
+let hash = (x, y) => {
+    return x + 60013 * y;
+}
+
 var robotSim = function(commands, obstacles) {
     let x = 0, y = 0;
     let direction = "N";
     let distance = 0;
     let max = 0;
     let obstaclesMap = new Map();
+    // [[2, 4]]
+    // [[1, 3], [3,6]]
+    for (let i = 0; i < obstacles.length; i++) {
+        obstaclesMap.set(hash(obstacles[i][0], obstacles[i][1]), true);
+    }
+
+    console.log(obstaclesMap);
+    
 
     for (let i = 0; i < commands.length; i++) {
 
@@ -39,40 +51,40 @@ var robotSim = function(commands, obstacles) {
         else if (commands[i] > 0) {
             
             for (let j = 1; j < (commands[i] + 1); j++) {
-                let isObstacle = false;
+                // let isObstacle = false;
                 
                 if (direction == "N") {
 
-                    for (let k = 0; k < obstacles.length; k++) {
+                    // for (let k = 0; k < obstacles.length; k++) {
                         
-                        if (x == obstacles[k][0] && (y + 1) == obstacles[k][1]) {
-                            isObstacle = true;
-                            break;
-                        }
-                    }
+                    //     if (x == obstacles[k][0] && (y + 1) == obstacles[k][1]) {
+                    //         isObstacle = true;
+                    //         break;
+                    //     }
+                    // }
 
-                    if (isObstacle == true) {
-                        break;
-                    }
-                    else {
-                        y++;
-                    }
+                    // if (isObstacle == true) {
+                    //     break;
+                    // }
+                    // else {
+                    //     y++;
+                    // }
                 }
                 else if (direction == "E") {
-                    for (let k = 0; k < obstacles.length; k++) {
+                    // for (let k = 0; k < obstacles.length; k++) {
                         
-                        if ((x + 1) == obstacles[k][0] && y == obstacles[k][1]) {
-                            isObstacle = true;
-                            break;
-                        }
-                    }
+                    //     if ((x + 1) == obstacles[k][0] && y == obstacles[k][1]) {
+                    //         isObstacle = true;
+                    //         break;
+                    //     }
+                    // }
 
-                    if (isObstacle == true) {
-                        break;
-                    }
-                    else {
-                        x++;
-                    }
+                    // if (isObstacle == true) {
+                    //     break;
+                    // }
+                    // else {
+                    //     x++;
+                    // }
                 }
                 else if (direction == "S") {
                     for (let k = 0; k < obstacles.length; k++) {
