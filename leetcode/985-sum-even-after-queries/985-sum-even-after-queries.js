fe1e606049f7e3530a0889787e79a28698aacd1b
Umme Hani <uh.snigdha@gmail.com>
2024-12-23 17:32:47 +0600

    m

--- leetcode/985-sum-even-after-queries/985-sum-even-after-queries.js
+++ leetcode/985-sum-even-after-queries/985-sum-even-after-queries.js
@@ -8,11 +8,18 @@ var sumEvenAfterQueries = function(nums, queries) {
 
             if (queries[i][1] == j) {
                 nums[j] += queries[i][0];
+                // update the nums array for the next iteration;
             }
 
-            if (nums[j] % 2 == 0) 
+            if (nums[j] % 2 == 0) {
+                sumOfEven += nums[j];
+            }
         }
+
+        sumOfEvenArray.push(sumOfEven);
     }
+
+    return sumOfEvenArray;
 };
 
 console.log(sumEvenAfterQueries([1,2,3,4], [[1,0],[-3,1],[-4,0],[2,3]]))
