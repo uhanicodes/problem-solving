2d197116de086809360ad86a90fc3d7fa42a0f98
Umme Hani <uh.snigdha@gmail.com>
2024-12-23 17:20:48 +0600

    m

--- leetcode/985-sum-even-after-queries/985-sum-even-after-queries.js
+++ leetcode/985-sum-even-after-queries/985-sum-even-after-queries.js
@@ -2,28 +2,17 @@ var sumEvenAfterQueries = function(nums, queries) {
     let sumOfEven = 0;
     let sumOfEvenArray = [];
 
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < queries.length; i++) {
 
-        for (let j = 0; j < queries.length; j++) {
+        for (let j = 0; j < nums.length; j++) {
 
-            if (queries[j][1] == i) {
-                nums[i] = nums[i] + queries[j][0];
-                console.log(nums);
-                
-                for (let k = 0; k < nums.length; k++) {
-                    
-                    if (nums[k] % 2 == 0) {
-                        sumOfEven = sumOfEven + nums[i];
-                    }
-                    nums.push(nums[k]);
-                }
-                sumOfEvenArray.push(sumOfEven);
-
-                console.log(sumOfEven);
+            if (queries[i][1] == j) {
+                nums[j] += queries[i][0];
             }
-        }  
+
+            if (nums[j] % 2 == 0) 
+        }
     }
-    return sumOfEvenArray;
 };
 
 console.log(sumEvenAfterQueries([1,2,3,4], [[1,0],[-3,1],[-4,0],[2,3]]))
