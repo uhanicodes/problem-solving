3d8668434b1a12cc688f7a43c76a2fc219192776
Umme Hani <uh.snigdha@gmail.com>
2024-12-27 13:02:57 +0600

    m

--- leetcode/498-diagonal-traverse/498-diagonal-traverse.js
+++ leetcode/498-diagonal-traverse/498-diagonal-traverse.js
@@ -6,14 +6,23 @@ var findDiagonalOrder = function(mat) {
 
         for (let j = 0; j < i+1; j++) {
             
-
             if (i % 2 == 0) {
-                console.log(i-j, j);
+                //console.log(i-j, j);
                 diagonalElements.push(mat[i-j][j]);                
             }
             else {
-                console.log(j, i-j);
+                //console.log(j, i-j);
                 diagonalElements.push(mat[j][i-j]);
+            }
+        }
+
+        for (let k = mat.length - (i + 1); k > 0; k--) {
+            
+            if ((mat.length - k) % 2) {
+                console.log(k, (mat.length - k));
+            }
+            else {
+                console.log((mat.length - k), k);
             }
         }
     }
@@ -21,6 +30,6 @@ var findDiagonalOrder = function(mat) {
     return diagonalElements;
 };
 
-console.log(findDiagonalOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+//console.log(findDiagonalOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
 
 console.log(findDiagonalOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]));
