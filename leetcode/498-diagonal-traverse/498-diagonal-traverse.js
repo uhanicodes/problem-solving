51f6087ebe4523a0228b79366e6fa614beba4e0e
Umme Hani <uh.snigdha@gmail.com>
2024-12-27 23:56:08 +0600

    m

--- leetcode/498-diagonal-traverse/498-diagonal-traverse.js
+++ leetcode/498-diagonal-traverse/498-diagonal-traverse.js
@@ -2,7 +2,7 @@ var findDiagonalOrder = function(mat) {
     let diagonalElements = [];    
     
     for (let i = 0; i < mat.length; i++) {
-        console.log('i =',i);
+        //console.log('i =',i);
 
         for (let j = 0; j < i+1; j++) {
             
@@ -17,25 +17,25 @@ var findDiagonalOrder = function(mat) {
         }       
     }
 
-    for (let i = 0; i < mat.length; i++) {
+    for (let i = mat.length - 1; i > 0; i--) {
         console.log('i =',i);
 
-        for (let j = 0; j < i+1; j++) {
+        for (let j = 0; j < i; j++) {
             
             if (i % 2 == 0) {
-                //console.log(i-j, j);
+                console.log(i-j, j);
                 diagonalElements.push(mat[i-j][j]);                
             }
             else {
-                //console.log(j, i-j);
+                console.log(j, i-j);
                 diagonalElements.push(mat[j][i-j]);
             }
         }       
     }
-    
+
       return diagonalElements;
 };
 
-//console.log(findDiagonalOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+console.log(findDiagonalOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
 
-console.log(findDiagonalOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]));
+// console.log(findDiagonalOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]));
