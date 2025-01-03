a8705cd2e26138490f3afe58d4f532d595b0e6ac
Umme Hani <uh.snigdha@gmail.com>
2025-01-03 20:51:12 +0600

    m

--- leetcode/1701-average-waiting-time/1701-average-waiting-time.js
+++ leetcode/1701-average-waiting-time/1701-average-waiting-time.js
@@ -1,12 +1,18 @@
 let averageWaitingTime = (customers) => {
     let waitingTimes = [];
+    let orderFinishTime = 0;
 
     for (i = 0; i < customers.length; i++) {
 
-        //1.chef may be free while a customer arrives
-            //waiting time = time[i]
-        
-        //2.chef may be not free while a customer arrives
+        if (customers[i][0] < orderFinishTime) {
+            //2.chef may be not free while a customer arrives
             //waiting time = time[i] + (order finish time of i-1 - arrival time[i])
+            orderFinishTime += customers[i][1];
+        }
+        else {
+            //1.chef may be free while a customer arrives
+            //waiting time = time[i]
+            orderFinishTime = customers[i][0] + customers[i][1];
+        }
     }
 }
