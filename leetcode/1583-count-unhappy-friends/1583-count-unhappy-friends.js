f42e02a8c84483b34dc43bf248f0ba098407ef4d
Umme Hani <uh.snigdha@gmail.com>
2025-01-05 01:19:46 +0600

    m

--- leetcode/1583-count-unhappy-friends/1583-count-unhappy-friends.js
+++ leetcode/1583-count-unhappy-friends/1583-count-unhappy-friends.js
@@ -3,17 +3,8 @@ var unhappyFriends = function(n, preferences, pairs) {
     //1.traverse in each friends;
     //2.traverse in each preferences' length;
     //3.traverse in each pairs;
-    for (let i = 0; i < n.length; i++) {
+    for (let i = 0; i < pairs.length; i++) {
 
-        for (let j = 0; j < preferences[i].length; j++) {
-
-            for (let k = 0; k < pairs.length; k++) {
-
-                for (let l = 0; l < pairs[k].length; l++) {
-                               
-                }
-            }
-        }
     }
     return unHappyFriends;
 };
