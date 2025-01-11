acdb5b84bb6692b599e90dc9236c03d8e6fceb2b
Umme Hani <uh.snigdha@gmail.com>
2025-01-11 22:48:26 +0600

    m

---
+++ leetcode/2079-watering-plants/2079-watering-plants.js
@@ -0,0 +1,17 @@
+var wateringPlants = function(plants, capacity) {
+    let steps = 0;
+
+    for (let i = 0; i < plants.length; i++) {
+        capacity -= plants[i];
+        steps += i + 1;
+
+        if (capacity < plants[i]) {
+            capacity += plants[i];
+            steps += i + 1;
+        }
+    }
+
+    return steps;
+};
+
+console.log(wateringPlants([2,2,3,3], 5));
