3b569986a0f4d9088f19d3c8c9044f74580a4b07
Umme Hani <uh.snigdha@gmail.com>
2025-01-13 22:58:04 +0600

    m

--- leetcode/2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
+++ leetcode/2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
@@ -1,17 +1,17 @@
 var triangularSum = function(nums) {
-    let newNums = nums;
-
+    let newNums = [];
     
-    if (newNums.length > 1) {
+    if (nums.length > 1) {
         
-        for (let i = 0; i < newNums.length; i++) {
-            newNums.push((newNums[i] + newNums[i + 1]) % 10);
+        for (let i = 0; i < nums.length - 1; i++) {
+            newNums.push((nums[i] + nums[i + 1]) % 10);
         }
     }
     else {
         return newNums;
     }
-    // return newNums;
+    
+    return newNums;
 };
 
 console.log(triangularSum([1,2,3,4,5]));
