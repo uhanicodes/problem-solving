82a4d9189b14b3be48ce14584afbd1a25c215bb8
Umme Hani <uh.snigdha@gmail.com>
2025-01-21 21:56:52 +0600

    m

--- leetcode/2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
+++ leetcode/2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
@@ -1,21 +1,29 @@
 var arrayChange = function(nums, operations) {
-    let operationsMap = new Map(operations);
+    let numsMap = new Map();
 
-    while (operationsMap.size !== 0) {
-        
-        for (let i = 0; i < nums.length; i++) {
-            
-            if (operationsMap.has(nums[i])) {
-                let key = nums[i];
-                nums[i] = operationsMap.get(nums[i]);
-                operationsMap.delete(key);
-            }
-        }
+    for (let i = 0; i < nums.length; i++) {
+        numsMap.set(nums[i], i);
     }
 
-    return nums;
+    for (let i = 0; i < operations.length; i++) {
+        let x = numsMap.get(operations[i][0]);
+        numsMap.set(operations[i][1], x);
+        numsMap.delete(operations[i][0]);
+    }
+
+    let numsMap2 = new Map();
+
+    for (const entry of numsMap.entries()) {
+        numsMap2.set(entry[1], entry[0]);
+    }
+
+    let arr = [...numsMap2.entries()].sort();
+
+    return arr.map(x => x[1]);
 };
 
 console.log(arrayChange([1,2,4,6], [[1,3],[4,7],[6,1]])); //output : [3,2,7,1]
 
-console.log(arrayChange([1,2], [[1,3],[2,1],[3,2]]));
+// console.log(arrayChange([1,2], [[1,3],[2,1],[3,2]]));
+
+// console.log(arrayChange([1,2,3,4,5], [[5,7],[4,5],[5,9]]));
