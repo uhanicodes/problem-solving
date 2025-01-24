e116e3ce6b5c0ab20563cd35f775282dc6ca631a
Umme Hani <uh.snigdha@gmail.com>
2025-01-24 23:38:33 +0600

    m

--- leetcode/213-house-robber-II/213-house-robber-II.js
+++ leetcode/213-house-robber-II/213-house-robber-II.js
@@ -16,11 +16,17 @@ var rob = function(nums) {
     else if (nums.length == 1) {
         return nums[0];
     }
-    else if (nums.length == 3) {
+    else if (nums.length < 4) {
+        let max = 0;
 
         for (let i = 0; i < nums.length; i++) {
-            maxAmountOfRobbedMoney = Math.max(nums[i]);
+            
+            if (nums[i] > max) {
+                max = nums[i];
+            }
         }
+
+        return max;
     }
     else if (nums.length > 3 && nums.length % 2 == 0) {
         let oddSum = 0;
@@ -47,3 +53,11 @@ console.log(rob([2,3,2])); //output: 8, expected: 3
 console.log(rob([1,2,3,1])); // output: 1, expected: 4
 
 console.log(rob([1,2,3])); // output: 8, expected: 3
+
+console.log(rob([7]));
+
+console.log(rob([1,2]));
+
+console.log(rob([3,4,5,2,1,6]));
+
+console.log(rob([3,4,5,2,1]));
