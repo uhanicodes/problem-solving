847cfc30e53b06337a512d53609bfa9e1ba2ddee
Umme Hani <uh.snigdha@gmail.com>
2025-01-24 23:04:06 +0600

    m

--- leetcode/213-house-robber-II/213-house-robber-II.js
+++ leetcode/213-house-robber-II/213-house-robber-II.js
@@ -1,38 +1,42 @@
 var rob = function(nums) {
     let maxAmountOfRobbedMoney = 0;
     
-    for (let i = 0; i < nums.length; i++) {
+    if (nums.length > 3 && nums.length % 2 !== 0) {
 
-        if (nums.length == 1) {
-            maxAmountOfRobbedMoney += nums[i];
-        }
-        else if (nums.length == 2) {
-            continue;
-        }
-        else if (nums.length == 3) {
+        for (let i = 0; i < nums.length; i++) {
 
-            if (nums[i + 1] > nums[i]) {
-                maxAmountOfRobbedMoney += nums[i + 1];
-            }
-            else{
-                maxAmountOfRobbedMoney += nums[i];
-            }
-        }
-        else if (nums.length % 2 == 0) {
-            
-            if (i == 0 && i % 2 == 0) {
-                maxAmountOfRobbedMoney += nums[i];
-            }
-        }
-        else {
-
-            if (i == 0 && i == nums.length -1) {
+            if (i == 0 && i == nums.length - 1){
                 continue;
             }
             else if (i % 2 !== 0) {
                 maxAmountOfRobbedMoney += nums[i];
             }
         }
+    }
+    else if (nums.length == 1) {
+        return nums[0];
+    }
+    else if (nums.length == 3) {
+
+        for (let i = 0; i < nums.length; i++) {
+            maxAmountOfRobbedMoney = Math.max(nums[i]);
+        }
+    }
+    else if (nums.length > 3 && nums.length % 2 == 0) {
+        let oddSum = 0;
+        let evenSum = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+
+            if (i % 2 == 0) {
+                evenSum += nums[i];
+            }
+            else {
+                oddSum += nums[i];
+            }
+        }
+
+        maxAmountOfRobbedMoney = Math.max(evenSum, oddSum);
     }
 
     return maxAmountOfRobbedMoney;
