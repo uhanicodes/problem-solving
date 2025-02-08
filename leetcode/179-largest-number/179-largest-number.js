106cb60897579282e8ecd76b10e12bcf2784ad26
Umme Hani <uh.snigdha@gmail.com>
2025-02-08 23:31:39 +0600

    m

--- leetcode/179-largest-number/179-largest-number.js
+++ leetcode/179-largest-number/179-largest-number.js
@@ -26,11 +26,22 @@ var largestNumber = function(nums) {
         if ((a.length > b.length) && (a[b.length] < b[b.length -1])) {
             return 1;
         }
-                
+
+        if ((a.length < b.length) && (b[a.length] > a[a.length -1])) {
+            return 1;
+        }
+        
+        if  ((a.length < b.length) && (b[a.length] < a[a.length -1])) {
+            return -1;
+        }       
         return 0;
     })
     
-    return numsString;
+    for (let i = 0; i < numsString.length; i++) {
+        largestNumberString += numsString[i];
+    }
+
+    return largestNumberString;
 };
 
 console.log(largestNumber([12,11,12]));
@@ -42,3 +53,17 @@ console.log(largestNumber([3,30,34,5,9]));
 console.log(largestNumber([23, 234135631]));
 
 console.log(largestNumber([231, 23])); // not providing correct output.
+
+console.log(largestNumber([234, 23]));
+
+console.log(largestNumber([233, 23]));
+
+console.log(largestNumber([543, 5435]));
+
+console.log(largestNumber([40, 45, 34, 78, 25, 76, 34]));
+
+// [34323,3432], 
+// Output
+// "343233432"
+// Expected
+// "343234323"
