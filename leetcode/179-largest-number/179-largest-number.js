6c94721181cde4fb8327428f54e26faa682d621d
Umme Hani <uh.snigdha@gmail.com>
2025-02-08 12:37:57 +0600

    m

--- leetcode/179-largest-number/179-largest-number.js
+++ leetcode/179-largest-number/179-largest-number.js
@@ -1,139 +1,33 @@
 var largestNumber = function(nums) {
-    nums.sort((a, b) => b - a);
-    return nums;
+    let numsString = [];
+    let largestNumberString = '';
+
+    for (let i = 0; i < nums.length; i++) {
+       numsString.push(nums[i].toString());
+    }
+    
+    numsString.sort((a, b) => {
+
+        if (a.length == b.length) {
+
+            for (let i = 0; i < a.length; i++) {
+
+                if (a[i] > b[i]) {
+                    return -1;
+                }
+
+                if (a[i] < b[i]) {
+                    return 1;
+                }
+            }
+        }
+        
+        return 0;
+    })
+    
+    return numsString;
 };
 
-let numString = (n) => {
-    let remain = '';
+console.log(largestNumber([12,11,12]));
 
-    while (n !== 0) {
-        remain = remain + (n % 10);
-        n = Math.floor(n / 10);
-    }
-
-    return remain;
-}
-
-console.log(numString(12345));
-
-console.log(largestNumber([3,30,34,5,9]));
-
-// let arr = ["hello", "blo", "bio", "flow"];
-
-// arr.sort((a, b) => {
-//     // if the strings have different lengths
-//     if (a.length > b.length) {
-//         return -1;
-//     }
-
-//     if (a.length < b.length) {
-//         return 1;
-//     }
-
-//     // if the strings have the same length
-//     for (let i = 0; i < a.length; i++) {
-//         if (a[i] > b[i]) {
-//             return 1;
-//         }
-
-//         if (a[i] < b[i]) {
-//             return -1;
-//         }
-//     }
-
-//     return 0;
-// });
-
-// console.log(arr);
-
-arr = ["ab", "b", "kab", "pac", "rat", "pit"];
-
-arr.sort((a,b) => {
-
-    if (a[0] == 'p' && b[0] !== 'p') {
-        return -1;
-    }
-
-    if (a[0] !== 'p' && b[0] == 'p') {
-        return 1;
-    }
-
-    if ((a[0] == 'p' && b[0] == 'p') || (a[0] !== 'p' && b[0] !== 'p')) {
-        return 0;
-    }
-    return 0;
-});
-
-console.log(arr);
-
-let nums = [12, 15, 18, 19, 31, 44];
-
-nums.sort((a, b) => {
-
-    if ((a % 2 == 0 && b % 2 == 0) || (a % 2 == 1 && b % 2 == 1)) {
-
-        if (a > b) {
-            return 1;
-        }
-
-        if(a < b) {
-            return -1;
-        }
-    }
-
-    if (a % 2 == 0 && b % 2 == 1) {
-        return -1;
-    }
-
-    if (a % 2 == 1 && b % 2 == 0) {
-        return 1;
-    }
-
-    return 0;
-});
-
-console.log(nums);
-
-let students = [{
-  "name": "Abid Hasan",
-  "class": "2",
-  "roll": "12"
-}, 
-{
-  "name": "Jaid Hasan",
-  "class": "5",
-  "roll": "12"
-},
-{
-  "name": "Shahjahan",
-  "class": "2",
-  "roll": "10"
-},
-{
-  "name": "Abid Hasan",
-  "class": "5",
-  "roll": "9"
-}];
-
-students.sort((a, b) => {
-
-    if (a.class > b.class) {
-        return -1;
-    }
-
-    if (a.class < b.class) {
-        return 1;
-    }
-
-    if ((a.class == b.class) && (a.roll > b.roll)) {
-        return -1;
-    }
-
-    if (((a.class == b.class) && (a.roll < b.roll))) {
-        return 1;
-    }
-
-    return 0;
-});
-
-console.log(students);
+console.log(largestNumber([2346, 7892, 2343]));
