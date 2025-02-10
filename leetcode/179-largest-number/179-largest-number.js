0be2e9542e10a54710146c9c4b3bb1b3e76dd996
Umme Hani <uh.snigdha@gmail.com>
2025-02-10 22:23:04 +0600

    m

--- leetcode/179-largest-number/179-largest-number.js
+++ leetcode/179-largest-number/179-largest-number.js
@@ -1,55 +1,44 @@
 var largestNumber = function(nums) {
     let numsString = [];
     let largestNumberString = '';
+    let sum = 0;
 
     for (let i = 0; i < nums.length; i++) {
        numsString.push(nums[i].toString());
     }
-    
-    numsString.sort((a, b) => {
 
-        if ((a + b) > (b + a)) {
-            return -1;
-        }
-
-        if ((a + b) < (b + a)) {
-            return 1;
-        }
-
-        // for (let i = 0; i < Math.min(a.length, b.length); i++) {
-
-        //     if (a[i] > b[i]) {
-        //         return -1;
-        //     }
-
-        //     if (a[i] < b[i]) {
-        //         return 1;
-        //     }
-        // }
-
-        // if ((a.length > b.length) && (a[b.length] > b[b.length -1])) {
-        //     return -1;
-        // }
-
-        // if ((a.length > b.length) && (a[b.length] < b[b.length -1])) {
-        //     return 1;
-        // }
-
-        // if ((a.length < b.length) && (b[a.length] > a[a.length -1])) {
-        //     return 1;
-        // }
-        
-        // if  ((a.length < b.length) && (b[a.length] < a[a.length -1])) {
-        //     return -1;
-        // }       
-        return 0;
-    })
-    
-    for (let i = 0; i < numsString.length; i++) {
-        largestNumberString += numsString[i];
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
     }
+     
+    if (sum == 0) {
+        return '0';
+    }
+    else {
+        
+        numsString.sort((a, b) => {
 
-    return largestNumberString;
+            if (a == '0' && b == '0') {
+                return 0;
+            }
+
+            if ((a + b) > (b + a)) {
+                return -1;
+            }
+
+            if ((a + b) < (b + a)) {
+                return 1;
+            }
+
+            return 0;
+        })
+        
+        for (let i = 0; i < numsString.length; i++) {
+            largestNumberString += numsString[i];
+        }
+
+        return largestNumberString;
+    }
 };
 
 console.log(largestNumber([12,11,12]));
@@ -64,11 +53,13 @@ console.log(largestNumber([231, 23])); // not providing correct output.
 
 console.log(largestNumber([234, 23]));
 
-console.log(largestNumber([233, 23]));
+console.log(largestNumber([233, 23, 0]));
 
 console.log(largestNumber([543, 5435]));
 
 console.log(largestNumber([40, 45, 34, 78, 25, 76, 34]));
+
+console.log(largestNumber([0, 0, 0]));
 
 // [34323,3432], 
 // Output
