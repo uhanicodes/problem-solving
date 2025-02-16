50a25b262f770a6037b55851bb30873c3b8359fc
Umme Hani <uh.snigdha@gmail.com>
2025-02-16 23:06:08 +0600

    m

--- leetcode/49-group-anagrams/49-group-anagrams.js
+++ leetcode/49-group-anagrams/49-group-anagrams.js
@@ -1,11 +1,32 @@
 var groupAnagrams = function(strs) {
    let sortStrs = [];
+   let anagrams = [];
 
    for (let i = 0; i < strs.length; i++) {
         sortStrs[i] = strs[i].split('').sort().join('');
    }
 
-   return sortStrs;
+   for (let i = 0; i < sortStrs.length; i++) {
+      let x = [strs[i]];
+
+      for (let j = i + 1; j <sortStrs.length; j++) {
+
+         if (sortStrs[i] == sortStrs[j]) {
+            x.push(strs[j]);
+            delete strs[j];
+         }
+         else {
+            continue;
+         }
+      }
+
+      anagrams.push(x);
+      delete strs[i];
+      x = [];
+
+   }
+
+   return anagrams;
 };
 
 console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
