262c4f57b2852d05955bf01b715070b11a05cd9b
Umme Hani <uh.snigdha@gmail.com>
2025-03-03 01:22:34 +0600

    m

---
+++ leetcode/289-game-of-life/289-game-of-life.js
@@ -0,0 +1,28 @@
+const dx = [-1, -1, -1,  0, 0,  1, 1, 1];
+const dy = [-1,  0,  1, -1, 1, -1, 0, 1];
+
+var gameOfLife = function(board) {
+    let listOfNeighbors = [];
+    let currentState = [];
+
+    for (let i = 0; i < board.length; i++) {
+
+        for(let j = 0; j < board[i].length; j++) {
+            // listOfNeighbors.push(board[i-1][j-1], board[i-1][j], board[i-1][j+1], board[i][j-1], board[i][j+1], board[i+1][j-1], board[i+1][j], board[i+1][j+1])
+            console.log('source:', i, j);
+            
+            for (let k = 0; k < 8; k++) {
+                
+                if ((i + dx[k]) == -1 || (i + dx[k]) == board[i].length || (j + dy[k]) == -1 || (j + dy[k]) == board[i].length) {
+                    continue;
+                }
+                
+                listOfNeighbors.push(board[i + dx[k]][j + dy[k]]);
+            }
+        }
+    }
+
+    return listOfNeighbors;
+};
+
+console.log(gameOfLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
