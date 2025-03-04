e1b0de93a6f82ac9ec2a1b76b01908ee9e64aeaa
Umme Hani <uh.snigdha@gmail.com>
2025-03-04 15:23:37 +0600

    m

--- leetcode/289-game-of-life/289-game-of-life.js
+++ leetcode/289-game-of-life/289-game-of-life.js
@@ -2,32 +2,52 @@ const dx = [-1, -1, -1,  0, 0,  1, 1, 1];
 const dy = [-1,  0,  1, -1, 1, -1, 0, 1];
 
 var gameOfLife = function(board) {
-    let listOfNeighbors = new Array(board.length);
-    let currentState = [];
+    let newBoard = new Array(board.length);
 
-    for (let i = 0; i < listOfNeighbors.length; i++) {
-        listOfNeighbors[i] = [];
+    for (let i = 0; i < newBoard.length; i++) {
+        newBoard[i] = new Array(board[0].length);
     }
+
+    console.log(newBoard);
 
     for (let i = 0; i < board.length; i++) {
-
-        for(let j = 0; j < board[i].length; j++) {
-            // listOfNeighbors.push(board[i-1][j-1], board[i-1][j], board[i-1][j+1], board[i][j-1], board[i][j+1], board[i+1][j-1], board[i+1][j], board[i+1][j+1])
-            console.log('source:', i, j);
             
-            for (let k = 0; k < 8; k++) {
+            for (let j = 0; j < board[i].length; j++) { 
+                let sum = 0;
                 
-                if ((i + dx[k]) == -1 || (i + dx[k]) == board.length || 
-                    (j + dy[k]) == -1 || (j + dy[k]) == board[i].length) {
-                    continue;
+                for (k = 0; k < 8; k++) {
+                    
+                    if ((i + dx[k]) == -1 || (i + dx[k]) == board.length || 
+                        (j + dy[k]) == -1 || (j + dy[k]) == board[i].length) {
+                        continue;
+                    }
+                    else {
+                        sum += board[i + dx[k]][j + dy[k]];
+                    }
                 }
-                
-                listOfNeighbors[i].push(board[i + dx[k]][j + dy[k]]);
-            }
-        }
-    }
+                console.log(sum);
 
-    return listOfNeighbors;
+                if (board[i][j] == 1) {
+                    
+                    if (sum < 2) {
+                        newBoard[i][j] = 0;
+                    }
+                    else if (sum == 2 || sum == 3) {
+                       newBoard[i][j] = board[i][j];
+                    }
+                    else if (sum > 3) {
+                        newBoard[i][j] = 0;
+                    }
+                }
+                else if (board[i][j] == 0) {
+
+                    if (sum == 3) {
+                        newBoard[i][j] = 1;
+                    }
+                }
+            }
+    }
+   return newBoard;
 };
 
 console.log(gameOfLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
