5bc0471bda8eee4ae7b49d0499069818f90d2532
Umme Hani <uh.snigdha@gmail.com>
2025-03-05 23:44:50 +0600

    m

--- leetcode/189-rotate-array/189-rotate-array.js
+++ leetcode/189-rotate-array/189-rotate-array.js
@@ -1,24 +1,12 @@
 var rotate = function(nums, k) {
-    let reverseNums = [];
-
-    for (let i = 0; i < nums.length; i++) {
-        reverseNums[i] = nums[nums.length - 1 - i];
-    }
-
     let x = 0;
-    for (let i = 0; i < k; i++) {
-        x = reverseNums[0];
-        reverseNums.splice(0, 1);
-        // console.log(reverseNums);
-        reverseNums.push(x);
-    }
 
-    let reverseReverseNums = [];
-    for (let i = 0; i < reverseNums.length; i++) {
-        reverseReverseNums[i] = reverseNums[reverseNums.length -1 - i];
+    for (let i = 0; i < k; i++) {
+        x = nums[nums.length - 1];
+        nums.splice(nums.length - 1, 1);
+        nums.splice(0, 0, x);
     }
-    return reverseReverseNums;
-};
+}
 
 console.log(rotate([1,2,3,4,5,6,7], 3));
 console.log(rotate([-1,-100,3,99], 2));
