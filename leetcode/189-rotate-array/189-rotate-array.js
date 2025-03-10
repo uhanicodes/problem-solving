ddf2fb5b604e0e019cb81b052b4486eaf51ee693
Umme Hani <uh.snigdha@gmail.com>
2025-03-10 23:17:57 +0600

    m

--- leetcode/189-rotate-array/189-rotate-array.js
+++ leetcode/189-rotate-array/189-rotate-array.js
@@ -1,96 +1,33 @@
-// var rotate = function(nums, k) {
-//     k = k % nums.length;
-
-//     let x = [];
-//     let y = [];
-
-//     for (let i = 0; i < nums.length - k; i++) {
-//         x.push(nums[i]);
-//         // console.log(x);
-//     }
-
-//     for (let i = nums.length - k; i < nums.length; i++) {
-//         y.push(nums[i]);
-//         // console.log(y);
-//     }
-
-//     for (let i = 0; i < k; i++) {
-//         nums[i] = y[i];
-//     }
-
-//     for (let i = k; i <nums.length; i++) {
-//         nums[i] = x[i - k];
-//     }
-    
-//     return nums;
-// }
-
-class Queue {
-    
-    constructor () {
-        this.array = [];
-        this.start = this.end = -1;
-    }
-
-    enqueue(v) {
-
-        if (this.start == -1 && this.end == -1) {
-            this.start = 0;
-        }
-
-        this.end++;
-        this.array[this.end] = v;
-    }
-
-    dequeue() {
-        let firstValue = 0;
-
-        if (this.start !== -1 && this.end !== -1) {
-            firstValue = this.array[this.start];
-            this.start++;
-        }
-
-        if (this.start - 1 == this.end) {
-            this.start = -1;
-            this.end = -1;
-        }
-
-        return firstValue;
-    }
-
-    isEmpty() {
-        
-        if (this.start == -1 && this.end == -1) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-}
-
 var rotate = function(nums, k) {
-    // k = k % nums.length;
-    let numsQueue = new Queue();
+    k = k % nums.length;
 
-    for (let i = 0; i < nums.length; i++) {
-        numsQueue.enqueue(nums[i]);
+    let x = [];
+    let y = [];
+
+    for (let i = 0; i < nums.length - k; i++) {
+        x.push(nums[i]);
+        // console.log(x);
+    }
+
+    for (let i = nums.length - k; i < nums.length; i++) {
+        y.push(nums[i]);
+        // console.log(y);
+    }
+
+    for (let i = 0; i < k; i++) {
+        nums[i] = y[i];
+    }
+
+    for (let i = k; i <nums.length; i++) {
+        nums[i] = x[i - k];
     }
     
-    for (let i = 0; i < k; i++) {
-        numsQueue.enqueue(numsQueue.dequeue());
-    }
-
-    for (let i = 0; i < k; i++) {
-        numsQueue.dequeue();
-    }
-
-    return numsQueue;
+    return nums;
 }
-console.log(rotate([1,2,3,4,5,6,7], 3));
-// console.log(rotate([-1,-100,3,99], 2));
-// console.log(rotate([1,2], 3));
-// console.log(rotate([1,2,3],50));
+
+console.log(rotate([1,2,3,4,5,6,7], 3)); // output: [5,6,7,1,2,3,4]
+console.log(rotate([-1,-100,3,99], 2)); // output: [3,99,-1,-100]
+console.log(rotate([1,2], 3)); // output: [2,1]
+console.log(rotate([1,2,3],50)); // output: [2,3,1]
 
 // nums = [1,2,3,4,5,6,7], k = 3
-// output: [5,6,7,1,2,3,4]
