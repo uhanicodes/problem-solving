a269b6c915187a41401d5f220c3daf33ffc4d634
Umme Hani <uh.snigdha@gmail.com>
2025-03-21 12:42:16 +0600

    m

--- leetcode/200-number-of-islands/200-number-of-islands.js
+++ leetcode/200-number-of-islands/200-number-of-islands.js
@@ -42,14 +42,16 @@ class Queue {
     }
 }
 
-let bfs = (grid) => {
+var numIslands = function(grid) {
 
     let visited = new Array(grid.length);
 
     for (let i = 0; i < grid.length; i++) {
+
+        visited[i] = [];
      
         for (let j = 0; j < grid[i].length; j++) {
-            visited[i][j] = false;
+            visited[i].push(false);
         }
     }
 
@@ -59,6 +61,10 @@ let bfs = (grid) => {
     for (let i = 0; i < grid.length; i++) {
 
         for (let j = 0; j < grid[i].length; j++) {
+
+            if (grid[i][j] == "0") {
+                continue;
+            }
             
             if (visited[i][j] == true) {
                 continue;
@@ -77,25 +83,49 @@ let bfs = (grid) => {
             while (!queue.isEmpty()) {
                 let u = queue.dequeue();
 
-                if (visited[u] == false) {
-                    visited[u] = true;
-                    path.push(u);
+                if (visited[u.x][u.y] == false) {
+                    visited[u.x][u.y] = true;
                 }
 
-                let adj = graph[u];
+                let dx = [0, 1, -1, 0];
+                let dy = [1, 0, 0, -1];
 
-                for (let i = 0; i < adj.length; i++) {
-                    let v = adj[i];
+                for (let k = 0; k < 4; k++) {
+                    let newX = u.x + dx[k];
+                    let newY = u.y + dy[k];
 
-                    if (!visited[v]) {
-                        queue.enqueue(v);
+                    if (newX == -1 || newY == -1 || newX == grid.length || newY == grid[i].length) {
+                        // cell may go outside the max index
+                        continue;
+                    }
+
+                    // console.log(newX, newY);
+
+                    if (grid[newX][newY] == "1" && !visited[newX][newY]) {
+                        queue.enqueue({
+                            x: newX,
+                            y: newY
+                        })
                     }
                 }
             }
         }
         
-        paths.push(path);
     }
 
-    return paths;
+    return island;
 }
+
+console.log(numIslands([
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]));
+
+console.log(numIslands([
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]));
