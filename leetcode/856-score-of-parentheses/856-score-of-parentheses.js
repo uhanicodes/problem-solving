530c2f43fce83756828de7fa1ef1835b0102340a
Umme Hani <uh.snigdha@gmail.com>
2025-04-05 22:43:44 +0600

    m

--- leetcode/856-score-of-parentheses/856-score-of-parentheses.js
+++ leetcode/856-score-of-parentheses/856-score-of-parentheses.js
@@ -1,11 +1,54 @@
+class Stack {
+
+    constructor() {
+        this.items = [];
+    }
+
+    push(number) {
+        this.items.push(number);
+    }
+
+    pop() {
+        
+        if (this.items === 0)
+            return 'The stack is empty!'
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+};
+
 var scoreOfParentheses = function(s) {
-    let score = 0;
+    let score = new Stack();
 
     for (let i = 0; i < s.length; i++) {
+    
+        if (s[i] == '(') {
+            score.push(0);
+        }
 
         if (s[i] == ')') {
-            score++;
-        }
+
+            if (s[i - 1] == '(') {
+                score.pop();
+                score.push(1);
+            }
+            else {
+                while (score.pop() !== 0) {
+                    score.push(score.pop() + score.pop());
+                }
+            }
+        }        
     }
     return score;
 };
